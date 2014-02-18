21420f629c6a332f15544101680dcee3de09eb53
wangkai <393867916@qq.com>
2014-02-19 00:12:36 +0800

    2014219

--- src/com/example/hello/MainActivity.java
+++ src/com/example/hello/MainActivity.java
@@ -5,19 +5,24 @@ import com.example.base.BaseUi;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends BaseUi {
-
+	private EditText editText;
+	private EditText editPass;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//设置模板界面
         
         //判断当前用户是否登录
         if(BaseAuth.isLogin()){//若登录则跳转
         	this.forward(IndexActivity.class);
         }
         //控件对象初始化，及记住密码
+        editText = (EditText) this.findViewById(R.id.editText1);//登陆账号
+        editPass = (EditText) this.findViewById(R.id.editText2);//登录密码
         
     }
 
