ba7c2fde3dc9d0dbba4bf93216fc6536b281ee7e
wang5815355 <393867916@qq.com>
2015-07-10 18:23:57 +0800

    123

--- src/com/hello008/hello/RegisterVerifyActivity.java
+++ src/com/hello008/hello/RegisterVerifyActivity.java
@@ -6,14 +6,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
-import com.hello008.base.BaseAuth;
-import com.hello008.base.BaseUi;
-import com.hello008.hello.MainActivity.AnsyTry;
-import com.hello008.model.Customer;
-import com.hello008.util.AppClient;
-
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.IntentFilter;
@@ -23,7 +16,11 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.telephony.TelephonyManager;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,11 +28,20 @@ import cn.smssdk.EventHandler;
 import cn.smssdk.SMSSDK;
 import cn.smssdk.gui.SMSReceiver;
 
+import com.hello008.base.BaseAuth;
+import com.hello008.base.BaseUi;
+import com.hello008.model.Customer;
+import com.hello008.util.AppClient;
+
 public class RegisterVerifyActivity extends BaseUi {
 	EditText phonenumberv;
 	TextView verifyv;
+	Button reload;
 	private TimeCount time;
 	String phonenumber;
+	String verifycode_s;
+	EventHandler eh;
+	SMSReceiver smsReceiver;
 	
 	
 	/**
@@ -45,7 +51,7 @@ public class RegisterVerifyActivity extends BaseUi {
 	private void initSms(){
 		//调用短信验证码 初始化
 		SMSSDK.initSDK(this,"8993354f32b8","c8ea55c1f8ca7b67042c1f76131f2514");
-		 EventHandler eh=new EventHandler(){
+		 eh = new EventHandler(){
 	            @Override
 	            public void afterEvent(int event, int result, Object data) {
 	               if (result == SMSSDK.RESULT_COMPLETE) {
@@ -75,6 +81,20 @@ public class RegisterVerifyActivity extends BaseUi {
 		
 		phonenumberv = (EditText) findViewById(R.id.phonenumber);
 		verifyv = (TextView) findViewById(R.id.verifycode);
+		reload = (Button) findViewById(R.id.btn_reload);
+		time = new TimeCount(3000, 1000);//计时器初始化
+		
+		reload.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				phonenumber = phonenumberv.getText().toString();
+				initSms();
+				SMSSDK.getVerificationCode("86",phonenumber, null);
+				time.start();
+				reload.setVisibility(8);
+				verifyv.setVisibility(0);
+			}
+		});
 		
 	    TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
 	    phonenumber = tm.getLine1Number();
@@ -82,21 +102,27 @@ public class RegisterVerifyActivity extends BaseUi {
 	    	phonenumber = phonenumber.substring(3);
 	    	phonenumberv.setText(phonenumber); 
 	    	phonenumberv.setSelection(phonenumber.length());
+	    	initSms();
+			SMSSDK.getVerificationCode("86",phonenumber, null);
+	    }else{
+	    	reload.setVisibility(0);
+			verifyv.setVisibility(8);
+			verifyv.setText("验证手机号码");
 	    }
-	    time = new TimeCount(20000, 1000);//计时器初始化
-	    initSms();
+	  
         // 获取验证码短信
 	    Log.w("testsms", phonenumber);
-		SMSSDK.getVerificationCode("86",phonenumber, null);
+	
 	    
-		SMSReceiver smsReceiver = new SMSReceiver(new SMSSDK.VerifyCodeReadListener() {
+		smsReceiver = new SMSReceiver(new SMSSDK.VerifyCodeReadListener() {
 			@Override
 			public void onReadVerifyCode(final String verifyCode) {
 				//启动轮询service
 				 new Handler().postDelayed(new Runnable(){  
-				     public void run() {  
+
+					public void run() {  
 //				    	 time.cancel();
-				    	 verifyv.setText("验证码:"+verifyCode);
+				    	 verifycode_s = verifyCode;
 				    	 HashMap<String, String> map = new HashMap<String, String>();
 				    	 // 获取验证码短信
 				 	     Log.w("testsms","+1"+ phonenumber);
@@ -124,11 +150,14 @@ public class RegisterVerifyActivity extends BaseUi {
 		}
 		@Override
 		public void onFinish() {//计时完毕时触发
-			
+			reload.setVisibility(0) ;
+			verifyv.setVisibility(8);
+			reload.setText("未收到验证短信 点击重新验证");
+			SMSSDK.unregisterEventHandler(eh);
 		}
 		@Override
 		public void onTick(long millisUntilFinished){//计时过程显示
-			verifyv.setText("自动验证手机号码  "+millisUntilFinished /1000+"秒");
+			verifyv.setText("自动验证手机号码  " + millisUntilFinished /1000 + "秒");
 		}
 	}
 	
@@ -180,7 +209,10 @@ public class RegisterVerifyActivity extends BaseUi {
 	    		try {
 	    			info = jo.getString("info");
 	    			status = jo.getString("status");
-	    			sid = jo.getString("sid");
+	    			if(status.equalsIgnoreCase("1")){
+	    				sid = jo.getString("sid");
+	    			}
+	    			
 	    		} catch (JSONException e) {
 	    			// TODO Auto-generated catch block
 	    			e.printStackTrace();
@@ -194,27 +226,24 @@ public class RegisterVerifyActivity extends BaseUi {
 				}
 	    	}
 			
-			
-			if(status.equals("5")){//当注册成功
-				dialog.dismiss();
+			if(status.equals("1")){//当注册成功
 				BaseAuth.setLogin(true);
 				setting = getSharedPreferences("register", Context.MODE_PRIVATE);
 				SharedPreferences.Editor editor = setting.edit();
 				editor.putBoolean("islogin", true);
-				editor.putString("username",hmap.get("username"));
-				editor.putString("password",hmap.get("password"));
-				editor.putString("regstep","1");//注册步骤 1，提交资料，2 提交头像姓名
+				editor.putString("phonenumber",hmap.get("phonenumber"));
+				editor.putString("password",hmap.get("phonenumber").substring(5));
+				editor.putString("regstep","1");//注册步骤 1， 验证码验证，2 提交头像姓名
 				editor.commit(); 
 				
 				Customer customer = Customer.getInstance();
 				customer.setSid(sid);//设置sessionid
+				verifyv.setText("验证码:"+verifycode_s);
+				SMSSDK.unregisterEventHandler(eh);
 				RegisterVerifyActivity.this.forward(RegisterTwoActivity.class);
-				Toast.makeText(RegisterVerifyActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
-			}else{//注册不成功
-				BaseAuth.setLogin(true);
-//				progress.dismiss();
-				dialog.cancel();
-				Toast.makeText(RegisterVerifyActivity.this,info,Toast.LENGTH_SHORT).show();
+				Toast.makeText(RegisterVerifyActivity.this,"验证成功",Toast.LENGTH_SHORT).show();
+			}else{//注册不成功  
+				Toast.makeText(RegisterVerifyActivity.this,info,Toast.LENGTH_LONG).show();
 			}
 			
 	       }
